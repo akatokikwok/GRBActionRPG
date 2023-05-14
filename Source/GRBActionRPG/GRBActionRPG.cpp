56dc69366d3b5566993bd471e0b39a0e6bfa2885
DESKTOP-6OVVB6Q\Askyr1m <akatokikwok@outlook.com>
2023-05-14 23:37:22 +0800

    [Init]: 首次初始化项目并忽略中间件文件.

---
+++ Source/GRBActionRPG/GRBActionRPG.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GRBActionRPG.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GRBActionRPG, "GRBActionRPG" );
---
+++ Source/GRBActionRPG/GRBActionRPGGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GRBActionRPGGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GRBACTIONRPG_API AGRBActionRPGGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
