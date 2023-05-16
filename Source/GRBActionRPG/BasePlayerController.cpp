62be1e142da625dd477e0f6cf5474e5a346cbe06
DESKTOP-6OVVB6Q\Askyr1m <akatokikwok@outlook.com>
2023-05-16 22:39:16 +0800

    [feat]: 制作人物的基础移动和输入组件.

---
+++ Source/GRBActionRPG/BasePlayerController.cpp
@@ -0,0 +1,46 @@
+﻿#include "BasePlayerController.h"
+#include "GameFramework/Character.h"
+
+
+ABasePlayerController::ABasePlayerController(const FObjectInitializer& ObjectInitializer /*= FObjectInitializer::Get()*/)
+	: Super(ObjectInitializer)
+{
+
+}
+
+void ABasePlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	this->InputComponent->BindAxis(TEXT("MoveForward"), this, &ABasePlayerController::MoveForward);
+	this->InputComponent->BindAxis(TEXT("MoveRight"), this, &ABasePlayerController::MoveRight);
+}
+
+void ABasePlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
+
+	if (this->MoveInput.IsNearlyZero()) {
+		return;
+	}
+
+	const FRotator Rot = this->GetControlRotation();
+	const FRotator YawRot(0, Rot.Yaw, 0);// YawRot就是控制器yaw的欧拉角
+	ACharacter* InCharacter = this->GetCharacter();
+	if (InCharacter == nullptr) {
+		return;
+	}
+	const FVector DirX = FRotationMatrix(YawRot).GetUnitAxis(EAxis::X);
+	InCharacter->AddMovementInput(DirX, MoveInput.X);
+	const FVector DirY = FRotationMatrix(YawRot).GetUnitAxis(EAxis::Y);
+	InCharacter->AddMovementInput(DirY, MoveInput.Y);
+}
+
+void ABasePlayerController::MoveForward(float Value)
+{
+	this->MoveInput.X = Value;
+}
+
+void ABasePlayerController::MoveRight(float Value)
+{
+	this->MoveInput.Y = Value;
+}
---
+++ Source/GRBActionRPG/BasePlayerController.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "BasePlayerController.generated.h"
+
+
+/**
+ *
+ */
+UCLASS()
+class GRBACTIONRPG_API ABasePlayerController : public APlayerController
+{
+	GENERATED_BODY()
+public:
+	/** Default Constructor */
+	ABasePlayerController(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+	virtual void SetupInputComponent() override;
+	virtual void PlayerTick(float DeltaTime) override;
+
+protected:
+	UFUNCTION()
+		void MoveForward(float Value);
+
+	UFUNCTION()
+		void MoveRight(float Value);
+
+private:
+	FVector MoveInput;
+};
