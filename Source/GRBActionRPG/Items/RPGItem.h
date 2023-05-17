a0ee6b38dc35847ab60fd1215e857aa01b301150
DESKTOP-6OVVB6Q\Askyr1m <akatokikwok@outlook.com>
2023-05-17 22:47:26 +0800

    [arch]: 设计RPGItem(1种PrimaryDataAsset), RPGItemData, RPGItemSlot的数据结构概念.

---
+++ Source/GRBActionRPG/Items/RPGItem.h
@@ -0,0 +1,71 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "Styling/SlateBrush.h"
+#include "RPGItem.generated.h"
+
+
+/**
+ * 所有Item资产的基类, 抽象基类,不可用于蓝图实例化.
+ */
+UCLASS(Abstract, BlueprintType)
+class GRBACTIONRPG_API URPGItem : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	/** Constructor */
+	URPGItem()
+		: Price(0)
+		, MaxCount(1)
+		, MaxLevel(1)
+		, AbilityLevel(1)
+	{}
+
+	/** 核验本Item资产是否负担得起数量消耗; Returns if the item is consumable (MaxCount <= 0)*/
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Max")
+		bool IsConsumable() const;
+
+	/** Returns the logical name, equivalent to the primary asset id */
+	UFUNCTION(BlueprintCallable, Category = "Item")
+		FString GetIdentifierString() const;
+
+	/** 拿取Item的 PrimaryAssetId */
+	virtual FPrimaryAssetId GetPrimaryAssetId() const override;
+
+protected:
+	/** Type of this item, set in native parent class */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item")
+		FPrimaryAssetType ItemType;
+
+	/** User-visible short name */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item")
+		FText ItemName;
+
+	/** User-visible long description */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item")
+		FText ItemDescription;
+
+	/** Icon to display */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item")
+		FSlateBrush ItemIcon;
+
+	/** Price in game */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item")
+		int32 Price;
+
+	/** Maximum number of instances that can be in inventory at once, <= 0 means infinite */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Max")
+		int32 MaxCount;
+
+	/** Maximum level this item can be, <= 0 means infinite */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Max")
+		int32 MaxLevel;
+
+	/** Ability level this item grants. <= 0 means the character level */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Abilities")
+		int32 AbilityLevel;
+};
+
+
---
+++ Source/GRBActionRPG/RPGTypes.h
@@ -0,0 +1,113 @@
+﻿#pragma once
+#include "UObject/PrimaryAssetId.h"
+#include "RPGTypes.generated.h"
+
+/**
+ * 代表1个Item的插槽,可显示在UI内
+ */
+USTRUCT(BlueprintType)
+struct GRBACTIONRPG_API FRPGItemSlot
+{
+	GENERATED_USTRUCT_BODY()
+public:
+	/** Constructor, -1 means an invalid slot */
+	FRPGItemSlot()
+		: SlotNumber(-1)
+	{}
+
+	FRPGItemSlot(const FPrimaryAssetType& InItemType, int32 InSlotNumber)
+		: ItemType(InItemType)
+		, SlotNumber(InSlotNumber)
+	{}
+
+	/** 物品的类型 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
+		FPrimaryAssetType ItemType;
+
+	/** 槽的索引号 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
+		int32 SlotNumber;
+
+	/** 重载操作符 */
+	bool operator==(const FRPGItemSlot& Other) const
+	{
+		return ItemType == Other.ItemType && SlotNumber == Other.SlotNumber;
+	}
+	bool operator!=(const FRPGItemSlot& Other) const
+	{
+		return !(*this == Other);
+	}
+
+	/** 为了在诸如Map的容器内使用,所以设立1个哈希方法; Implemented so it can be used in Maps/Sets */
+	friend inline uint32 GetTypeHash(const FRPGItemSlot& Key)
+	{
+		uint32 Hash = 0;
+		Hash = HashCombine(Hash, GetTypeHash(Key.ItemType));
+		Hash = HashCombine(Hash, (uint32)Key.SlotNumber);
+		return Hash;
+	}
+
+	/** Returns true if slot is valid */
+	bool IsValid() const
+	{
+		return ItemType.IsValid() && SlotNumber >= 0;
+	}
+};
+
+
+/** Extra information about a URPGItem that is in a player's inventory */
+USTRUCT(BlueprintType)
+struct GRBACTIONRPG_API FRPGItemData
+{
+	GENERATED_USTRUCT_BODY()
+public:
+	/** Constructor, default to count/level 1 so declaring them in blueprints gives you the expected behavior */
+	FRPGItemData()
+		: ItemCount(1)
+		, ItemLevel(1)
+	{}
+
+	FRPGItemData(int32 InItemCount, int32 InItemLevel)
+		: ItemCount(InItemCount)
+		, ItemLevel(InItemLevel)
+	{}
+
+	/** The number of instances of this item in the inventory, can never be below 1 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
+		int32 ItemCount;
+
+	/** The level of this item. This level is shared for all instances, can never be below 1 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
+		int32 ItemLevel;
+
+	/** Equality operators */
+	bool operator==(const FRPGItemData& Other) const
+	{
+		return ItemCount == Other.ItemCount && ItemLevel == Other.ItemLevel;
+	}
+	bool operator!=(const FRPGItemData& Other) const
+	{
+		return !(*this == Other);
+	}
+
+	/** Returns true if count is greater than 0 */
+	bool IsValid() const
+	{
+		return ItemCount > 0;
+	}
+
+	/** Append an item data, this adds the count and overrides everything else */
+	void UpdateItemData(const FRPGItemData& Other, int32 MaxCount, int32 MaxLevel)
+	{
+		if (MaxCount <= 0) {
+			MaxCount = MAX_int32;
+		}
+
+		if (MaxLevel <= 0) {
+			MaxLevel = MAX_int32;
+		}
+
+		ItemCount = FMath::Clamp(ItemCount + Other.ItemCount, 1, MaxCount);
+		ItemLevel = FMath::Clamp(Other.ItemLevel, 1, MaxLevel);
+	}
+};
